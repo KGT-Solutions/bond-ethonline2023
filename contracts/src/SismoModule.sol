014d9fa27f32a112bdf08fbd10c84e8a52a12aa0
nigsdev <nigsdev@proton.me>
2023-10-16 18:20:33 +0530

    install required dependencies

---
+++ contracts/src/SismoModule.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+// import SismoConnect Solidity library
+import "sismo-connect-solidity/SismoLib.sol";
+
+/**
+ * @title Bond
+ * @author nigsdev
+ */
+ contract Enum {
+    enum Operation {
+        Call,
+        DelegateCall
+    }
+}
+
+interface GnosisSafe {
+    /// @dev Allows a Module to execute a Safe transaction without any further confirmations.
+    /// @param to Destination address of module transaction.
+    /// @param value Ether value of module transaction.
+    /// @param data Data payload of module transaction.
+    /// @param operation Operation type of module transaction.
+    function execTransactionFromModule(
+        address to,
+        uint256 value,
+        bytes calldata data,
+        Enum.Operation operation
+    ) external returns (bool success);
+
+    function addOwnerWithThreshold(address owner, uint256 threshold) external;
+}
+
+
+contract SismoModule is SismoConnect {
+    bytes16 public groupId;
+    GnosisSafe public safe;
+
+    //TODO make proxyable w/ initializer
+    constructor(address _safe, bytes16 _appId, bytes16 _groupId) SismoConnect(_appId) {
+        safe = GnosisSafe(_safe);
+        groupId = _groupId;
+    }
+
+    /// @dev Change group identifier
+    /// @param _groupId group identifier to check the claim
+    function setGroupId(bytes16 _groupId) public {
+        require(msg.sender == address(safe), "!safe");
+        groupId = _groupId;
+    }
+
+
+    /// @dev Exec tx using zkConnect proof
+    /// @param to Destination address of module transaction
+    /// @param value Ether value of module transaction
+    /// @param data Data payload of module transaction
+    /// @param operation Operation type of module transaction
+    function execTransactionFromModule(
+        address to,
+        uint256 value,
+        bytes calldata data,
+        Enum.Operation operation,
+        bytes memory zkConnectResponse
+    ) public virtual returns (bool success) {
+        SismoConnectVerifiedResult memory result = verify({
+            responseBytes: zkConnectResponse,
+            auth: buildAuth({authType: AuthType.VAULT}),
+            claim: buildClaim({groupId: groupId}),
+            signature: buildSignature({message: abi.encode(to, value, data, operation)})
+        });
+
+        require(safe.execTransactionFromModule(to, value, data, operation), "Module transaction failed");
+
+        return true;
+    }
+
+}
