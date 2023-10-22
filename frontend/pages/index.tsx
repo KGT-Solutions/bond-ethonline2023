dcb4ce061d92323b1d73dd8418fac7b02b0c1ea3
nigsdev <nigsdev@proton.me>
2023-10-22 20:52:58 +0530

    updted frontend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,16 +1,15 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-
 "use client";
-
 import {
   SismoConnectButton,
   AuthType,
   SismoConnectResponse,
   ClaimType,
 } from "@sismo-core/sismo-connect-react";
+
+// import { ConnectButton } from '@rainbow-me/rainbowkit';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -25,7 +24,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <ConnectButton />
+        {/* <ConnectButton /> */}
 
         <h1 className={styles.title}>
           Welcome to <a href="">Bond</a>
@@ -38,24 +37,24 @@ const Home: NextPage = () => {
         <SismoConnectButton
           config={{
             appId: "0xf5831918dad952c25dc4b8b45ee1d45f", // replace with your appId
-            vault: {
-              // For development purposes insert the Data Sources that you want to impersonate here
-              // Never use this in production
-              impersonate: [
-                // EVM
-                "leo21.sismo.eth",
-                "0xA4C94A6091545e40fc9c3E0982AEc8942E282F38",
-                "0x1b9424ed517f7700e7368e34a9743295a225d889",
-                "0x82fbed074f62386ed43bb816f748e8817bf46ff7",
-                "0xc281bd4db5bf94f02a8525dca954db3895685700",
-                // Github
-                "github:leo21",
-                // Twitter
-                "twitter:leo21_eth",
-                // Telegram
-                "telegram:leo21",
-              ],
-            },
+            // vault: {
+            //   // For development purposes insert the Data Sources that you want to impersonate here
+            //   // Never use this in production
+            //   impersonate: [
+            //     // EVM
+            //     "leo21.sismo.eth",
+            //     "0xA4C94A6091545e40fc9c3E0982AEc8942E282F38",
+            //     "0x1b9424ed517f7700e7368e34a9743295a225d889",
+            //     "0x82fbed074f62386ed43bb816f748e8817bf46ff7",
+            //     "0xc281bd4db5bf94f02a8525dca954db3895685700",
+            //     // Github
+            //     "github:leo21",
+            //     // Twitter
+            //     "twitter:leo21_eth",
+            //     // Telegram
+            //     "telegram:leo21",
+            //   ],
+            // },
             // displayRawResponse: true,
           }}
           // request proof of Data Sources ownership (e.g EVM, GitHub, twitter or telegram)
@@ -67,7 +66,7 @@ const Home: NextPage = () => {
             { groupId: "0x0f800ff28a426924cbe66b67b9f837e2" }, 
           ]} 
           // request message signature from users.
-          signature={{ message: "I vote Yes to Privacy" }}
+          signature={{ message: "I am verifying with Github with Privacy." }}
           // retrieve the Sismo Connect Reponse from the user's Sismo data vault
           onResponse={async (response: SismoConnectResponse) => {
             const res = await fetch("/api/verify", {
