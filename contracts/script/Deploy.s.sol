4ceeb2a4dd6f18c97602232dd45fe27b9d49673d
nigsdev <nigsdev@proton.me>
2023-10-22 19:59:14 +0530

    configuration changes

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -10,15 +10,15 @@ contract DeploySismoModule is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         
-        bytes16 _appId = 0x282ff775a754ebea339746f096635a5a;
+        bytes16 _appId = 0xf5831918dad952c25dc4b8b45ee1d45f;
         VaultConfig memory _vault;
         _vault.isImpersonationMode = false;
 
-        address _safe = 0x0911BA4aE8D1cbC39A3f42bC9F91B06c0d681609;
+        address _safe = 0xcBA7EF4F7a5e75b2B1bc377684db48bA41516378;
         SismoConnectConfig memory _config;
         _config.appId = _appId;
         _config.vault = _vault;
-        bytes16 _groupId = 0x84f8495423ea1aa1b212356f31f6c7d9;
+        bytes16 _groupId = 0x0f800ff28a426924cbe66b67b9f837e2; //ENS Owners
 
         SismoModule sismoConnectModule = new SismoModule(_safe, _config, _groupId);
         console.log("sismoconnectmodule delpoyed to", address(sismoConnectModule));
